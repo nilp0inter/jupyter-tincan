a9ceb5592caf6671250249f1c828f1c9fcf6731c
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-12-02 02:26:51 +0100

    fix: working multiline text

--- jupyter_tincan/html.py
+++ jupyter_tincan/html.py
@@ -11,6 +11,8 @@ class HTML2SVG:
         soup = BeautifulSoup(html, 'html.parser')
         for text_element in soup.find_all(text=True):
             if text_element.parent.name not in ['script', 'style']:  # Skip script and style tags
-                new_text = self.text2svg(text_element)
+                lines = text_element.splitlines()
+                lines = [f"<span>{self.text2svg(line)}</span>" for line in lines]
+                new_text = "<br>".join(lines)
                 text_element.replace_with(BeautifulSoup(new_text, 'html.parser'))
         return str(soup)
